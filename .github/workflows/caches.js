bc643cb1bf86a593b2e244ea06a8930df2b4995f
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-01-15 02:33:14 -0600

    Debug

--- .github/workflows/caches.js
+++ .github/workflows/caches.js
@@ -13,12 +13,12 @@ module.exports = {
       `!node_modules/*/package-lock.json`,
     ],
   },
-  npm: {
-    path: [`${process.env.HOME}/.npm`],
-    hashFiles: [
-      `package-lock.json`,
-      `*/*/package-lock.json`,
-      `!node_modules/*/package-lock.json`,
-    ],
-  },
+  // npm: {
+  //   path: [`${process.env.HOME}/.npm`],
+  //   hashFiles: [
+  //     `package-lock.json`,
+  //     `*/*/package-lock.json`,
+  //     `!node_modules/*/package-lock.json`,
+  //   ],
+  // },
 };
