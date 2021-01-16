803bd30295d14d6e6579fde40f557e07bc7e54f8
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-01-16 13:34:00 -0600

    Fix path

--- .github/workflows/caches.js
+++ .github/workflows/caches.js
@@ -1,30 +1,46 @@
+const os = require("os");
+const platform = os.platform();
+const { HOME = "~", GITHUB_WORKSPACE } = process.env;
+
+const pathByPlatform = {
+  linux: {
+    pip: `${HOME}/.cache/pip`,
+  },
+  darwin: {
+    pip: `${HOME}/Library/Caches/pip`,
+  },
+  win32: {
+    pip: `${HOME}\\AppData\\Local\\pip\\Cache`,
+  },
+};
+
 module.exports = {
   pip: {
-    path: [`${process.env.HOME}/.cache/pip`],
+    path: pathByPlatform[platform].pip,
     hashFiles: ["requirements.txt"],
     keyPrefix: "pip-",
     restoreKeys: "pip-",
   },
   npm: {
-    path: [`${process.env.HOME}/.npm`],
+    path: `${HOME}/.npm`,
     hashFiles: ["package-lock.json"],
     keyPrefix: "npm-",
     restoreKeys: "npm-",
   },
   npmdev: {
-    path: [`${process.env.HOME}/.npm`],
+    path: `${HOME}/.npm`,
     hashFiles: ["package-lock.json"],
     keyPrefix: "npmdev-",
     restoreKeys: "npmdev-",
   },
   bundler: {
-    path: [`${process.env.GITHUB_WORKSPACE}/vendor/bundle/ruby/3.0.0/cache`],
+    path: `${GITHUB_WORKSPACE}/vendor/bundle/ruby/3.0.0/cache`,
     hashFiles: ["Gemfile.lock"],
     keyPrefix: "bundler-",
     restoreKeys: "bundler-",
   },
   gem: {
-    path: ["/opt/hostedtoolcache/Ruby/3.0.0/x64/lib/ruby/gems/3.0.0/cache"],
+    path: "/opt/hostedtoolcache/Ruby/3.0.0/x64/lib/ruby/gems/3.0.0/cache",
     keyPrefix: "gem-",
     restoreKeys: "gem-",
   },
