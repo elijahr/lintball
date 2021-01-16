ac58b0c30464a58f69a18fddf072ffd0108c4cde
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-01-16 14:08:53 -0600

    Fix node 15

--- .github/workflows/caches.js
+++ .github/workflows/caches.js
@@ -1,5 +1,5 @@
-const os = require("os");
-const platform = os.platform();
+import { platform } from "os";
+
 const { HOME = "~", GITHUB_WORKSPACE } = process.env;
 
 const pathByPlatform = {
@@ -14,35 +14,33 @@ const pathByPlatform = {
   },
 };
 
-module.exports = {
-  pip: {
-    path: pathByPlatform[platform].pip,
-    hashFiles: ["requirements.txt"],
-    keyPrefix: "pip-",
-    restoreKeys: "pip-",
-  },
-  npm: {
-    path: `${HOME}/.npm`,
-    hashFiles: ["package-lock.json"],
-    keyPrefix: "npm-",
-    restoreKeys: "npm-",
-  },
-  npmdev: {
-    path: `${HOME}/.npm`,
-    hashFiles: ["package-lock.json"],
-    keyPrefix: "npmdev-",
-    restoreKeys: "npmdev-",
-  },
-  bundler: {
-    path: `${GITHUB_WORKSPACE}/vendor/bundle/ruby/3.0.0/cache`,
-    hashFiles: ["Gemfile.lock"],
-    keyPrefix: "bundler-",
-    restoreKeys: "bundler-",
-  },
-  gem: {
-    path: "/opt/hostedtoolcache/Ruby/3.0.0/x64/lib/ruby/gems/3.0.0/cache",
-    hashFiles: [],
-    keyPrefix: "gem-",
-    restoreKeys: "gem-",
-  },
+export const pip = {
+  path: pathByPlatform[platform()].pip,
+  hashFiles: ["requirements.txt"],
+  keyPrefix: "pip-",
+  restoreKeys: "pip-",
+};
+export const npm = {
+  path: `${HOME}/.npm`,
+  hashFiles: ["package-lock.json"],
+  keyPrefix: "npm-",
+  restoreKeys: "npm-",
+};
+export const npmDev = {
+  path: `${HOME}/.npm`,
+  hashFiles: ["package-lock.json"],
+  keyPrefix: "npmDev-",
+  restoreKeys: "npmDev-",
+};
+export const bundler = {
+  path: `${GITHUB_WORKSPACE}/vendor/bundle/ruby/3.0.0/cache`,
+  hashFiles: ["Gemfile.lock"],
+  keyPrefix: "bundler-",
+  restoreKeys: "bundler-",
+};
+export const gem = {
+  path: "/opt/hostedtoolcache/Ruby/3.0.0/x64/lib/ruby/gems/3.0.0/cache",
+  hashFiles: [],
+  keyPrefix: "gem-",
+  restoreKeys: "gem-",
 };
