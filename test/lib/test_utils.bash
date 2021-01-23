b3628de7f06022f0d4a5a735393aa4559ead883f
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-01-22 23:13:53 -0600

    fix project_dir

--- test/lib/test_utils.bash
+++ test/lib/test_utils.bash
@@ -1,15 +1,13 @@
-PROJECT_DIR="$(realpath "$(dirname "$BATS_TEST_DIRNAME")")"
+PROJECT_DIR="$(
+  cd "$(dirname "$BATS_TEST_DIRNAME")"
+  pwd
+)"
 export PROJECT_DIR
 
 LINTBALL_DIR="$PROJECT_DIR"
 export LINTBALL_DIR
 
-# ORIGINAL_PATH="$PATH"
-# export ORIGINAL_PATH
-
 setup_test() {
-  # PATH="${LINTBALL_DIR}/bin:$PATH"
-  # export PATH
   TEST_PROJECT_DIR="$(mktemp -d)/fixture"
   export TEST_PROJECT_DIR
   cp -r "${LINTBALL_DIR}/test/fixture/" "${TEST_PROJECT_DIR}/"
@@ -22,8 +20,6 @@ setup_test() {
 teardown_test() {
   rm -rf "$(dirname "$TEST_PROJECT_DIR")"
   unset TEST_PROJECT_DIR
-  # PATH="$ORIGINAL_PATH"
-  # export PATH
 }
 
 git_branch() {
